5d150b299abdd5f469cb576312638190207f2c0e
Shambhavi <shambhavi102006@gmail.com>
2026-06-22 21:56:50 +0530

    Added calculator program

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def add (a,b):
+    return a + b
+def subtract (a,b):
+    return a - b
+
+num1 = int(input("Enter a number :"))
+num2 = int(input("Enter a number:"))
+
+print("Addition =", add(num1,num2))
+print("Subtraction =", subtract(num1,num2))
